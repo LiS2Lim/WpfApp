28b2d0b16980033d0f605c25bef47ff75353e3f5
LiS2Lim <s2alua.fu@gmail.com>
2023-09-06 18:01:52 +0900

    Add image files

--- View/LoginView.xaml.cs
+++ View/LoginView.xaml.cs
@@ -31,5 +31,15 @@ namespace WpfApp.View
                 DragMove();
             }
         }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
     }
 }
